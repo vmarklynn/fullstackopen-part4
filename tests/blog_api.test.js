405c2cd84233817f1f76afa6131bd25f99a52a62
Vincent Marklynn <56606238+vmarklynn@users.noreply.github.com>
2023-08-24 13:44:48 -0700

    Separated tests into describe blocks

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -8,89 +8,90 @@ const Blog = require("../models/blog");
 beforeEach(async () => {
   // Clear existing data in MongoDB
   await Blog.deleteMany({});
-  console.log("Cleared collections in MongoDB");
-
-  const blogs = helper.initialBlogs.map((blog) => new Blog(blog));
-  const promiseArray = blogs.map((blog) => blog.save());
-
-  await Promise.all(promiseArray);
+  await Blog.insertMany(helper.initialBlogs);
 });
 
-test("MongoDB returns JSON data", async () => {
-  await api
-    .get("/api/blogs")
-    .expect(200)
-    .expect("Content-Type", /application\/json/);
-});
+describe("tests where MongoDB contains previous values", () => {
+  test("MongoDB returns JSON data", async () => {
+    await api
+      .get("/api/blogs")
+      .expect(200)
+      .expect("Content-Type", /application\/json/);
+  });
 
-test("Test all notes are returned", async () => {
-  const response = await api.get("/api/blogs");
-  expect(response.body).toHaveLength(helper.initialBlogs.length);
-});
+  test("Test all blogs are returned", async () => {
+    const response = await api.get("/api/blogs");
+    expect(response.body).toHaveLength(helper.initialBlogs.length);
+  });
 
-test("Test id is defined", async () => {
-  const blogs = await Blog.find({});
-  blogs.forEach((blog) => {
-    expect(blog.id).toBeDefined();
+  test("Test id is defined", async () => {
+    const blogs = await Blog.find({});
+    blogs.forEach((blog) => {
+      expect(blog.id).toBeDefined();
+    });
   });
 });
 
-test("Test post", async () => {
-  const newBlog = {
-    title: "Software Developers are working too much.",
-    author: "J. Blake",
-    url: "www.jblock.com",
-    likes: 200,
-  };
+describe("POST requests", () => {
+  test("Test post", async () => {
+    const newBlog = {
+      title: "Software Developers are working too much.",
+      author: "J. Blake",
+      url: "www.jblock.com",
+      likes: 200,
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const blogsAfterPost = await helper.blogsFromDb();
-  expect(blogsAfterPost).toHaveLength(helper.initialBlogs.length + 1);
+    const blogsAfterPost = await helper.blogsFromDb();
+    expect(blogsAfterPost).toHaveLength(helper.initialBlogs.length + 1);
 
-  const titles = blogsAfterPost.map((blog) => blog.title);
-  expect(titles).toContain("Software Developers are working too much.");
+    const titles = blogsAfterPost.map((blog) => blog.title);
+    expect(titles).toContain("Software Developers are working too much.");
+  });
 });
 
-test("Test post without likes will default to 0", async () => {
-  const newBlog = {
-    title: "Software Developers are working too much.",
-    author: "J. Blake",
-    url: "www.jblock.com",
-  };
+describe("Verify validations and default values", () => {
+  test("Test post without likes will default to 0", async () => {
+    const newBlog = {
+      title: "Software Developers are working too much.",
+      author: "J. Blake",
+      url: "www.jblock.com",
+    };
 
-  await api
-    .post("/api/blogs")
-    .send(newBlog)
-    .expect(201)
-    .expect("Content-Type", /application\/json/);
+    await api
+      .post("/api/blogs")
+      .send(newBlog)
+      .expect(201)
+      .expect("Content-Type", /application\/json/);
 
-  const blogsAfterPost = await helper.blogsFromDb();
-  const likes = blogsAfterPost.map((blog) => blog.likes);
+    const blogsAfterPost = await helper.blogsFromDb();
+    const likes = blogsAfterPost.map((blog) => blog.likes);
 
-  expect(likes).toContain(0);
-});
+    expect(likes).toContain(0);
+  });
 
-test("Test blog without title will return 400", async () => {
-  const newBlogWoutTitle = {
-    author: "Jack Bean",
-    url: "www.beanstalk.com",
-  };
+  test("Test blog without title will return 400", async () => {
+    const newBlogWoutTitle = {
+      author: "Jack Bean",
+      url: "www.beanstalk.com",
+    };
 
-  await api.post("/api/blogs").send(newBlogWoutTitle).expect(400);
-});
+    await api.post("/api/blogs").send(newBlogWoutTitle).expect(400);
+  });
 
-test("Test blog without url will return 400", async () => {
-  const newBlogWoutUrl = {
-    title: "How to win friends and alienate people",
-    author: "Jack Bean",
-  };
+  test("Test blog without url will return 400", async () => {
+    const newBlogWoutUrl = {
+      title: "How to win friends and alienate people",
+      author: "Jack Bean",
+    };
 
-  await api.post("/api/blogs").send(newBlogWoutUrl).expect(400);
+    await api.post("/api/blogs").send(newBlogWoutUrl).expect(400);
+  });
 });
 
 afterAll(async () => {
